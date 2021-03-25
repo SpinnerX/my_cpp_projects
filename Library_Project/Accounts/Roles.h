f50b196dfe6134e3af2c9faaff65fa7a7c44d1a8
Aaron Her <aaronher@aarons-mbp.attlocal.net>
2021-03-24 23:00:28 -0700

    Users Positions File

---
+++ Library_Project/Accounts/Roles.h
@@ -0,0 +1,11 @@
+#ifndef LIBRARY_SYSTEM_PROJECT_ROLES_H
+#define LIBRARY_SYSTEM_PROJECT_ROLES_H
+#pragma once
+#include "Roles.h"
+
+enum class Roles{
+    Admin,
+    Employee,
+    Member
+};
+#endif //LIBRARY_SYSTEM_PROJECT_ROLES_H
